b3643530db97dc7ff5c0fac5e79006f11dda0f76
Ashley Ruglys <ashley.ruglys@gmail.com>
2023-09-14 21:58:16 +1200

    Referenced prims are loaded in

--- blender_modify_usd.py
+++ blender_modify_usd.py
@@ -1,41 +1,57 @@
 from pxr import Usd, UsdGeom, UsdShade, Sdf, Gf, Tf
 import bpy
+from bpy.props import StringProperty
+from bpy_extras.io_utils import ImportHelper
+from bpy.types import Operator
 import copy
+import os
+import itertools
 
 stage = None
 object_to_prim = {}
 
+def move_objects_between_collections(objects, from_col, to_col):
+    for object in objects:
+        to_col.objects.link(object)
+        from_col.objects.unlink(object)
+
 def load(filename):
     global stage
     object_to_prim.clear()
+    prim_to_object = {}
     stage = Usd.Stage.Open(filename)
     cache = UsdGeom.XformCache()
-    stack = [(stage.GetDefaultPrim(), None)]
+
     prototype_collection = bpy.data.collections.new("prototypes")
+    prototype_collection.hide_viewport = True
     bpy.context.scene.collection.children.link(prototype_collection)
 
     reference_collections = {}
 
-    # Todo: we can probably just traverse the stage normally but
-    # this makes setting the parent object slightly easier.
-    while len(stack) > 0:
-        (prim, parent_obj) = stack.pop()
-
+    for prim in stage.Traverse():
         object = bpy.data.objects.new(str(prim.GetPath()), None)
         object.matrix_basis = list(cache.GetLocalTransformation(prim)[0])
         object_to_prim[object] = [prim, copy.copy(object.matrix_basis)]
+        prim_to_object[prim] = object
+
+        prim_parent = prim.GetParent()
+        if prim_parent and prim_parent in prim_to_object:
+            object.parent = prim_to_object[prim_parent]
+
         bpy.context.scene.collection.objects.link(object)
 
-
-        if parent_obj is not None:
-            object.parent = parent_obj
+        # Todo: we want to import prims if they're not referenced at all.
+        #if prim.GetTypeName() == "Mesh":
+        #    bpy.ops.wm.usd_import(filepath = filename, prim_path_mask = str(prim.GetPath()))
 
         arcs = Usd.PrimCompositionQuery.GetDirectReferences(prim).GetCompositionArcs()
 
         if len(arcs) > 0:
             direct_ref = arcs[0]
-            rel_path_from_base_dir = os.path.relpath(direct_ref.GetTargetLayer().realPath, os.path.dirname(stage.GetRootLayer().realPath))
-            collection_name = rel_path_from_base_dir + direct_ref.GetTargetPrimPath().pathString
+            filepath = direct_ref.GetTargetLayer().realPath
+            prim_path = direct_ref.GetTargetPrimPath().pathString
+            rel_path_from_base_dir = os.path.relpath(filepath, os.path.dirname(stage.GetRootLayer().realPath))
+            collection_name = rel_path_from_base_dir + prim_path
 
             object.instance_type = "COLLECTION"
 
@@ -46,13 +62,19 @@ def load(filename):
                 prototype_collection.children.link(collection)
                 reference_collections[collection_name] = collection
                 object.instance_collection = collection
-        else:
-            for child in prim.GetChildren():
-                stack.append((child, object))
+
+                #if prim.IsInstance():
+                # Import the referenced prims into the new collection
+                bpy.ops.wm.usd_import(filepath = filepath, prim_path_mask = prim_path)
+                move_objects_between_collections(bpy.context.selected_objects, bpy.context.scene.collection, collection)
 
 def save():
     for object, (prim, original_matrix) in object_to_prim.items():
-        if object.matrix_basis == original_matrix:
+        try:
+            if object.matrix_basis == original_matrix:
+                continue
+        except Exception as e:
+            print(e)
             continue
 
         original_pos, original_rot, original_scale = original_matrix.decompose()
@@ -73,23 +95,36 @@ def save():
         elif types == ["xformOp:translate"]:
             if pos_modifed:
                 xform_ops[0].Set(Gf.Vec3d(list(pos)))
+            if rot_modified:
+                prim.AddXformOp(UsdGeom.XformOp.TypeOrient).Set(Gf.Quatd(*list(rot)))
+            if scale_modified:
+                prim.AddXformOp(UsdGeom.XformOp.TypeScale).Set(Gf.Vec3d(list(scale)))
         elif types == ["xformOp:translate", "xformOp:orient", "xformOp:scale"]:
             if pos_modifed:
                 xform_ops[0].Set(Gf.Vec3d(list(pos)))
             if rot_modified:
                 xform_ops[1].Set(Gf.Quatd(*list(rot)))
             if scale_modified:
-                pass
-                #xform_ops[2].Set(Gf.Vec3d(list(scale)))
+                xform_ops[2].Set(Gf.Vec3d(list(scale)))
         elif types == ["xformOp:translate", "xformOp:rotateXYZ", "xformOp:scale"]:
+
             if pos_modifed:
-                xform_ops[0].Set(Gf.Vec3d(list(pos)))
+                pass
+                #xform_ops[0].Set(Gf.Vec3d(list(pos)))
             # Todo: quaternion to xyz.
             if rot_modified:
                 pass
                 #xform_ops[1].Set(Gf.Quatd(*list(rot)))
             if scale_modified:
-                xform_ops[2].Set(Gf.Vec3d(list(scale)))
+                pass
+                #xform_ops[2].Set(Gf.Vec3d(list(scale)))
+            prim.ClearXformOpOrder()
+            prim.AddXformOp(UsdGeom.XformOp.TypeTranslate).Set(Gf.Vec3d(list(pos)))
+            prim.AddXformOp(UsdGeom.XformOp.TypeOrient).Set(Gf.Quatd(*list(rot)))
+            try:
+                prim.AddXformOp(UsdGeom.XformOp.TypeScale).Set(Gf.Vec3d(list(scale)))
+            except Exception as e:
+                print(e)
         elif types == []:
             if pos_modifed:
                 prim.AddXformOp(UsdGeom.XformOp.TypeTranslate).Set(Gf.Vec3d(list(pos)))
@@ -118,22 +153,25 @@ def save():
             for x in unsupported:
                 if types == x:
                     is_known_unsupported = True
-            assert is_known_unsupported
+            #assert is_known_unsupported
 
-            #prim.ClearXformOpOrder()
-            #prim.AddXformOp(UsdGeom.XformOp.TypeTranslate).Set(Gf.Vec3d(list(pos)))
-            #prim.AddXformOp(UsdGeom.XformOp.TypeOrient).Set(Gf.Quatd(*list(rot)))
-            #prim.AddXformOp(UsdGeom.XformOp.TypeScale).Set(Gf.Vec3d(list(scale)))
+            prim.ClearXformOpOrder()
+            prim.AddXformOp(UsdGeom.XformOp.TypeTranslate).Set(Gf.Vec3d(list(pos)))
+            prim.AddXformOp(UsdGeom.XformOp.TypeOrient).Set(Gf.Quatd(*list(rot)))
+            try:
+                prim.AddXformOp(UsdGeom.XformOp.TypeScale).Set(Gf.Vec3d(list(scale)))
+            except Exception as e:
+                print(e)
 
         stage.GetRootLayer().Save()
 
 class LoadScene(bpy.types.Operator):
-    bl_idname = "object.load_scene"        # Unique identifier for buttons and menu items to reference.
-    bl_label = "Load Scene"         # Display name in the interface.
+    bl_idname = "object.reload_scene"        # Unique identifier for buttons and menu items to reference.
+    bl_label = "Reload Scene"         # Display name in the interface.
     bl_options = {'REGISTER', 'UNDO'}  # Enable undo for the operator.
 
     def execute(self, context):        # execute() is called when running the operator.
-        load("C:\\Users\\Ashley\\Desktop\\Kitchen_set\\Kitchen_set_instanced.usd")
+        load("C:\\Users\\Ashley\\Desktop\\Kitchen_set\\Kitchen_set.usd")
 
         return {'FINISHED'}            # Lets Blender know the operator finished successfully.
 
@@ -147,6 +185,20 @@ class SaveScene(bpy.types.Operator):
 
         return {'FINISHED'}            # Lets Blender know the operator finished successfully.
 
+class OT_TestOpenFilebrowser(Operator, ImportHelper):
+
+    bl_idname = "object.filebrowser_usd"
+    bl_label = "Select a USD to load"
+
+    filter_glob: StringProperty(
+        default='*.usd*',
+        options={'HIDDEN'}
+    )
+
+    def execute(self, context):
+        load(self.filepath)
+
+        return {'FINISHED'}
 
 class SaveLoadPanel(bpy.types.Panel):
     bl_label = "Save/Load"
@@ -158,13 +210,13 @@ class SaveLoadPanel(bpy.types.Panel):
         layout = self.layout
 
         box = layout.box()
-        #box.label(text="Selection Tools")
-        #box.operator("object.select_all").action = 'TOGGLE'
-        row = box.row()
-        #row.operator("object.select_all").action = 'INVERT'
-        row.operator("object.load_scene")
-        row.operator("object.save_scene")
+        #row = box.row()
+        box.row().operator("object.filebrowser_usd")
+        #box.row().operator("object.reload_scene")
+        box.row().operator("object.save_scene")
+
 
 bpy.utils.register_class(LoadScene)
 bpy.utils.register_class(SaveScene)
 bpy.utils.register_class(SaveLoadPanel)
+bpy.utils.register_class(OT_TestOpenFilebrowser)
